2e5fa9eabab7936541a2c533ba0374c4dcb7c153
user <user@FD07>
2020-03-17 11:34:35 +0800

    完成小作業

--- src/haha/hello1.java
+++ src/haha/hello1.java
@@ -4,7 +4,12 @@ public class hello1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			String a = "haha";
+			System.out.print(a.charAt(0)+"%n");
+			String lyric = "Let it go! Let it go! Cannot hold it back anymore";
+			String b = lyric.replace("it", "her");
+			b = b.replace("Cannot", "Can't");
+			System.out.print(b);
 	}
 
 }
